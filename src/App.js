d2439b79cb999b83e3f5d9a01e6a3193bba9cdd0
Naser <108549635+naser0o0@users.noreply.github.com>
2023-06-24 21:45:00 +0200

    final

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Title from './components/Title';
 import Setup from "./examples/2-headers"; 
 // import PostRequest from "./examples/3-post-request"; 
 // import GlobalInstance from './examples/4-global-instance';
-import customInstance from './examples/5-custom-instance';
+import CustomInstance from "./examples/5-custom-instance";
 import './axios/global';
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       {/* <Setup/>   */}
       {/* <PostRequest/> */}
       {/* <GlobalInstance/> */}
-      <customInstance/>
+      <CustomInstance />
     </main>
   );
 }
